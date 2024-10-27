5a9ed7a58f5d9b8bfdd5144a02603ac42dfadc88
Tavrid-skif <akameumi@gmail.com>
2024-10-28 00:32:10 +0300

    Add files via upload

---
+++ PW_09/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PW_09
+{
+    internal class Program
+    {
+        static void PrintStudents(List<Student> students)
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        struct Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public double GPA { get; set; }
+
+            public override string ToString() { return $"ID: {Id}, Имя: {Name}, Возраст: {Age}, Средний бал: {GPA}"; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+            
+            students.Add(new Student { Id = 1, Name = "Иван Иванов", Age = 19, GPA = 3.8 });
+            students.Add(new Student { Id = 2, Name = "Мария Петрова", Age = 20, GPA = 3.5 });
+            students.Add(new Student { Id = 3, Name = "Сергей Сидоров", Age = 21, GPA = 3.2 });
+            students.Add(new Student { Id = 4, Name = "Анна Кузнецова", Age = 18, GPA = 4.0 });
+                        
+            Console.Write("Введите количество студентов (дополнительно): ");
+            if (int.TryParse(Console.ReadLine(), out int numStudents))
+            {
+                for (int i = 0 + 1; i <= numStudents; i++) 
+                {
+                    Console.Write($"\nВведите данные для студента {i}:\n");    
+
+                    Console.Write("ФИО: ");
+                    string name = Console.ReadLine();
+
+                    Console.Write("Возраст: ");
+
+                    if (int.TryParse(Console.ReadLine(), out int age))
+                    {
+                        Console.Write("Средний балл (GPA): ");
+                        if (double.TryParse(Console.ReadLine(), out double gpa))
+                        {
+                            int id = students.Count + 1;
+                            students.Add(new Student { Id = id, Name = name, Age = age, GPA = gpa });
+                        }
+                        else
+                        {
+                            Console.WriteLine("Неверный формат среднего балла.\n");
+                            i--;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неверный формат возраста.\n");
+                        i--; 
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Неверный формат количества студентов.\n");
+                return;
+            }
+
+            Console.WriteLine("\nНеотсортированный список студентов:");
+            PrintStudents(students);
+
+            students.Sort((s1, s2) => string.Compare(s1.Name, s2.Name)); 
+            Console.WriteLine("\nОтсортированный список студентов (по имени):");
+            PrintStudents(students);
+                        
+            students.Sort((s1, s2) => s1.Age.CompareTo(s2.Age)); 
+            Console.WriteLine("\nОтсортированный список студентов (по возрасту):");
+            PrintStudents(students);
+                       
+            students.Sort((s1, s2) => s2.GPA.CompareTo(s1.GPA)); 
+            Console.WriteLine("\nОтсортированный список студентов (по среднему баллу):");
+            PrintStudents(students);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PW_09/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PW_09")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PW_09")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("3d8c50d9-3057-40cf-b5c0-75f548e68f1d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
