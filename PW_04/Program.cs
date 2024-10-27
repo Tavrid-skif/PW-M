2c4a67230cffb517e05749bdaadd4d95a7428685
Tavrid-skif <akameumi@gmail.com>
2024-10-28 00:29:54 +0300

    Add files via upload

---
+++ PW_04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PW_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 6, recA = 7, recB = 16, triaA = 5, triaB = 12;
+
+            Console.WriteLine($"Площадь круга с радиусом ({radius}): {Shape.CalculateArea(radius)}\n");
+            Console.WriteLine($"Площадь прямоугольника со сторонами ({recA}, {recB}): {Shape.CalculateArea(recA, recB)}\n");
+            Console.WriteLine($"Площадь прямоугольного треугольника ({triaA}, {triaB}): {Shape.CalculateArea(triaA, triaB, true)}"); 
+            Console.WriteLine($"Площадь непрямоугольного треугольника ({triaA}, {triaB}, примерная третья сторона): {Shape.CalculateArea(triaA, triaB, false)}\n"); 
+        }
+    }
+}
---
+++ PW_04/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PW_04
+{
+    internal class Shape
+    {
+        static public double CalculateArea(double radius) { return Math.PI * Math.Pow(radius, 2); }
+        static public double CalculateArea(double width, double height) { return width * height; }
+        static public double CalculateArea(double baseLength, double height, bool isRightTriangle) { return isRightTriangle?0.5*baseLength*height: HeronArea(baseLength,height, Math.Sqrt(baseLength * baseLength + height * height)); }
+
+        static private double HeronArea(double a, double b, double c) {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+    }
+}
