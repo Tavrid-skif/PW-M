4d8e46822cfb043ea77b13cc464123c5d8b65a0e
Tavrid-skif <akameumi@gmail.com>
2024-10-28 00:30:48 +0300

    Add files via upload

---
+++ PW_05/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PW_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = 6, length = 7, width = 16, height = 5;
+
+            Console.WriteLine($"Объем шара с радиусом ({radius}): {VolumeCalculator.CalculateVolume(radius)}\n");
+            Console.WriteLine($"Площадь прямоугольного параллелепипеда со сторонами ({length}, {width}, {height}): {VolumeCalculator.CalculateVolume(length, width, height)}\n");
+            Console.WriteLine($"Объем цилиндра с радиусом и высотой ({radius}, {height}): {VolumeCalculator.CalculateVolume(radius, height)}");
+           
+        }
+    }
+}
---
+++ PW_05/VolumeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PW_05
+{
+    internal class VolumeCalculator
+    {
+
+        public static double CalculateVolume(double radius) { return (4 / 3) * Math.PI * Math.Pow(radius, 3); }
+
+        public static double CalculateVolume(double length, double width, double height) { return length * width * height; }
+
+        public static double CalculateVolume(double radius, double height) { return Math.PI * Math.Pow(radius, 2) * height; }
+    }
+}
